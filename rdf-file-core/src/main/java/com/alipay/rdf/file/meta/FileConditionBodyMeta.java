8fd9d5637903118a3e43bcbdf9a1f79f805dd4d9
jkyu@haiyi-info.com <jkyu@haiyi-info.com>
2018-09-29 17:29:36 +0800

    [init] 增加多个值匹配和正在表达式匹配的功能

--- rdf-file-core/src/main/java/com/alipay/rdf/file/meta/FileConditionBodyMeta.java
+++ rdf-file-core/src/main/java/com/alipay/rdf/file/meta/FileConditionBodyMeta.java
@@ -3,6 +3,7 @@ package com.alipay.rdf.file.meta;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * @author yujiakui
@@ -11,7 +12,12 @@ import java.util.Map;
  * date: 2018/9/29 13:38
  * description：文件body带条件的列元数据
  **/
-public class FileConditionBodyMeta implements Serializable{
+public class FileConditionBodyMeta implements Serializable {
+
+    /**
+     * 逗号分隔符号
+     */
+    private final static String SEP = ",";
 
     /**
      * serial id
@@ -35,13 +41,52 @@ public class FileConditionBodyMeta implements Serializable{
 
     /**
      * 根据行信息，判断当前行是否满足此模板
+     *
      * @param colInfos
      * @return
      */
-    public boolean satifyCnd(String[] colInfos){
-        for(FileColumnMeta fileColumnMeta:condColMetas){
+    public boolean satifyCnd(String[] colInfos) {
+        for (FileColumnMeta fileColumnMeta : condColMetas) {
             if (fileColumnMeta.getColIndex() >= bodyColMetas.size() ||
-                    !fileColumnMeta.getDefaultValue().equals(colInfos[fileColumnMeta.getColIndex()])){
+                    !match(fileColumnMeta.getDefaultValue(), colInfos[fileColumnMeta.getColIndex()])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 值匹配
+     *
+     * @param patternStr
+     * @param colValue
+     * @return
+     */
+    private boolean match(String patternStr, Object colValue) {
+        if (null == patternStr && null == colValue) {
+            return true;
+        }
+        // 使用逗号分隔
+        String[] splitValues = patternStr.split(SEP);
+        for (String splitValue : splitValues) {
+            // 使用正在表达式进行匹配
+            if (Pattern.matches(splitValue, colValue.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 根据行信息，判断当前行是否满足此模板
+     *
+     * @param colInfos
+     * @return
+     */
+    public boolean satifyCnd(Object[] colInfos) {
+        for (FileColumnMeta fileColumnMeta : condColMetas) {
+            if (fileColumnMeta.getColIndex() >= bodyColMetas.size() ||
+                    !match(fileColumnMeta.getDefaultValue(), colInfos[fileColumnMeta.getColIndex()])) {
                 return false;
             }
         }
@@ -50,27 +95,13 @@ public class FileConditionBodyMeta implements Serializable{
 
     /**
      * 根据行信息，判断当前行是否满足此模板
-     * @param colInfos
-     * @return
-     */
-    public boolean satifyCnd(Object[] colInfos){
-        for(FileColumnMeta fileColumnMeta:condColMetas){
-            if (fileColumnMeta.getColIndex() >= bodyColMetas.size() ||
-                    !fileColumnMeta.getDefaultValue().equals(colInfos[fileColumnMeta.getColIndex()])){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 根据行信息，判断当前行是否满足此模板
+     *
      * @param colInfoMap
      * @return
      */
-    public boolean satifyCnd(Map<String,Object> colInfoMap){
-        for(FileColumnMeta fileColumnMeta:condColMetas){
-            if (!fileColumnMeta.getDefaultValue().equals(colInfoMap.get(fileColumnMeta.getName()))){
+    public boolean satifyCnd(Map<String, Object> colInfoMap) {
+        for (FileColumnMeta fileColumnMeta : condColMetas) {
+            if (!fileColumnMeta.getDefaultValue().equals(colInfoMap.get(fileColumnMeta.getName()))) {
                 return false;
             }
         }
